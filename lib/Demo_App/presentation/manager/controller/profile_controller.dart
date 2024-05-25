29ff60a0ad012aec108565c2fea844c779bc0e2f
jishnukp2482 <jishnukpvidyadhran123@gmail.com>
2024-05-25 15:10:28 +0530

    apk full

--- lib/Demo_App/presentation/manager/controller/profile_controller.dart
+++ lib/Demo_App/presentation/manager/controller/profile_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:demo_application/Demo_App/domain/entities/profile_modal.dart';
 import 'package:demo_application/Demo_App/presentation/routes/app_pages.dart';
 import 'package:demo_application/Demo_App/presentation/themes/app_colors.dart';
+import 'package:demo_application/Demo_App/presentation/widgets/custom_alert_dialogue.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,7 +15,19 @@ class ProfileController extends GetxController {
     ProfileModal(Icon(Icons.grid_view_rounded), "Orders", () {
       Get.toNamed(AppPages.orderPage);
     }, AppColors.red),
-    ProfileModal(
-        Icon(Icons.key_outlined), "Logout", () {}, AppColors.iconcolor),
+    ProfileModal(Icon(Icons.key_outlined), "Logout", () {
+      customAlertDialogue(
+        title: "Exit!",
+        content: "Are you Sure You Want to Exit",
+        txtbutton1Action: () {
+          exit(0);
+        },
+        txtbutton2Action: () {
+          Get.back();
+        },
+        txtbuttonName1: "Yes",
+        txtbuttonName2: "No",
+      );
+    }, AppColors.iconcolor),
   ];
 }
