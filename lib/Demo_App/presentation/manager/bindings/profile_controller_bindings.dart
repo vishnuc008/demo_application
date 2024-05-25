8235c36dc1b70b861ebbd0aee976e45a8460690d
jishnukp2482 <jishnukpvidyadhran123@gmail.com>
2024-05-25 15:23:20 +0530

    apk

--- lib/Demo_App/presentation/manager/bindings/profile_controller_bindings.dart
+++ lib/Demo_App/presentation/manager/bindings/profile_controller_bindings.dart
@@ -1,10 +1,9 @@
 import 'package:demo_application/Demo_App/presentation/manager/controller/profile_controller.dart';
 import 'package:get/get.dart';
 
-class ProfileControllerBindings extends Bindings{
+class ProfileControllerBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => ProfileController());
   }
-
 }
